18b9efce7ffbaa58150bce3b61b3036cbc9beff4
NNaafff1 <nnffr660@gmail.com>
2025-11-24 02:19:46 +0300

    add boostrap

--- student/К3341/Саид_Наваф/Lr2/django_project_Саид_Наваф/project_first_app/templatetags/racing_filters.py
+++ student/К3341/Саид_Наваф/Lr2/django_project_Саид_Наваф/project_first_app/templatetags/racing_filters.py
@@ -9,3 +9,10 @@ def filter_by_race(registrations, race):
 @register.filter
 def filter_by_team(cars, team):
     return [car for car in cars if car.team == team]
+
+# Add a filter to add a CSS class to form fields
+from django import forms
+
+@register.filter(name='add_class')
+def add_class(field, css_class):
+    return field.as_widget(attrs={**field.field.widget.attrs, 'class': css_class})
